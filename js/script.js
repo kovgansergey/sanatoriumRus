2906bf3047c79a01f8c4f5466568001673747689
Сергей Ковган <skydiver.88@mail.ru>
2020-11-13 17:28:40 +0700

    сверстал форму для брони путевки, адаптировал, начал скрипт автоподстановки даты в инпуты

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-// функция изменения бургер-меню на мобильных экранах
+// скрипт изменения бургер-меню на мобильных экранах
 try {
   const menu = document.querySelector('.menu'),
     menuBurger = document.querySelector('.menu-burger');
@@ -15,3 +15,13 @@ try {
   console.error(error);
 }
 
+// скрипт для дат в форме брони путевки
+try {
+  const bookingFormArrival = document.getElementById('booking-form-arrival'),
+    bookingFormDeparture = document.getElementById('booking-form-departure'),
+    today = new Date();
+    
+  console.log(today.toDateString());
+} catch (error) {
+  console.error(error);
+}
